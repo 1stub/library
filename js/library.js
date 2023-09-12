a764a37c58e204663a1aacb484ad977c15b458cd
Anthony Arnold <stubbiegaming@gmail.com>
2023-09-12 11:34:56 -0400

    added delete button (need to add input validation

--- js/library.js
+++ js/library.js
@@ -10,11 +10,12 @@ const addBookBtn = document.createElement('button');
 
 const myLibrary = [];
 
-function Book(title, author, length, isRead){
+function Book(title, author, length, isRead, id){
     this.title = title;
     this.author = author;
     this.length = length;
     this.isRead = isRead;
+    this.id = id;
 }
 
 let count = 0;
@@ -59,21 +60,25 @@ function createBookButton(){
     popup.appendChild(addBookBtn);
 };    
 
+let globalRead = false;
 function createBook(){
     let bookName = document.getElementById('Name').value;
     let author = document.getElementById('Author').value;
     let pages = document.getElementById('Pages').value;
     let read = document.getElementById('isRead');
+    let id = myLibrary.length;
       if(read.checked === true) isRead = "Read";
       if(read.checked === false) isRead = "Not Read"
-    const book = new Book(bookName, author, pages, isRead);
+    const book = new Book(bookName, author, pages, isRead,id);
     myLibrary.push(book);
+    return globalRead = read.checked;
 }
 
 function populateLibrary(){
     let i = myLibrary.length-1;
     const book = document.createElement('div');
     const readButton = document.createElement('button');
+    const delButton = document.createElement('button');
     const bookInfo1 = document.createElement('p');
     const bookInfo2 = document.createElement('p');
     const bookInfo3 = document.createElement('p');
@@ -86,8 +91,20 @@ function populateLibrary(){
     book.id = i;
     readButton.classList = 'bookBtn';
     readButton.textContent = myLibrary[i].isRead;
-    readButton.style.backgroundColor = "red";
+    createReadButton(readButton);
+    createDelButton(delButton);
+    book.appendChild(bookInfo1);
+    book.appendChild(bookInfo2);
+    book.appendChild(bookInfo3);
+    book.appendChild(readButton);
+    book.appendChild(delButton);
+    lib.appendChild(book);
+}
+
+function createReadButton(e){
     (function(button) {
+        e.style.backgroundColor = "red";
+        if(globalRead === true) e.style.backgroundColor = "green";
         button.addEventListener('click', function(){
           console.log('Event listener added to button', button.textContent)
           const parent = button.parentNode;
@@ -95,15 +112,25 @@ function populateLibrary(){
           if (myLibrary[parentID].isRead === "Not Read") {
             myLibrary[parentID].isRead = "Read";
             button.textContent = "Read"; // Change the text content
-            readButton.style.backgroundColor = "green";
+            e.style.backgroundColor = "green";
           }
         });
-      })(readButton); // Pass readButton as an argument to the IIFE
-    book.appendChild(bookInfo1);
-    book.appendChild(bookInfo2);
-    book.appendChild(bookInfo3);
-    book.appendChild(readButton);
-    lib.appendChild(book);
+    })(e);// Pass readButton as an argument to the IIFE
+}
+
+function createDelButton(e){
+      (function(button) {
+        e.style.backgroundColor = "gray";
+        button.textContent = "Delete";
+        button.addEventListener('click', function(){
+          const parent = button.parentNode;
+          let indexParent = parent.id; 
+          const par = document.getElementById(indexParent);
+          par.parentNode.removeChild(par);
+          const id = myLibrary[indexParent].id;
+          myLibrary.splice(id, 1);
+        });
+  })(e);// Pass readButton as an argument to the IIFE
 }
 
 function createLable(lable){
