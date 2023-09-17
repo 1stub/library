772fa05d7b015f565577ed48fcafbddde7337809
Anthony Arnold <stubbiegaming@gmail.com>
2023-09-17 15:51:31 -0400

    test

---
+++ js/firebaseInfo.js
@@ -0,0 +1,9 @@
+export const firebaseInfo = {
+  apiKey: "AIzaSyBSB2R8VJYv0fpmx5F31eQKKDtylToMKk8",
+  authDomain: "library-a11c6.firebaseapp.com",
+  databaseURL: "https://library-a11c6-default-rtdb.firebaseio.com",
+  projectId: "library-a11c6",
+  storageBucket: "library-a11c6.appspot.com",
+  messagingSenderId: "37423042353",
+  appId: "1:37423042353:web:6bd0e8cdf00a033b42aa10"
+};    
